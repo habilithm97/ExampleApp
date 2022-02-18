607f9c267e940a88097e4976f570f863c158df9d
habilithm97 <habinkim97@gmail.com>
2022-02-18 23:39:05 +0900

    프래그먼트2(작성화면) 구현

--- app/src/main/java/com/example/exampleapp/ListFragment.java
+++ app/src/main/java/com/example/exampleapp/ListFragment.java
@@ -89,10 +89,10 @@ public class ListFragment extends Fragment {
         adapter = new CardAdapter();
 
         adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 공부", "0", "cube.jpg", "2월 17일"));
-        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 공부", "0", "cube.jpg", "2월 17일"));
-        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 공부", "0", "cube.jpg", "2월 17일"));
-        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 공부", "0", "cube.jpg", "2월 17일"));
-        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 공부", "0", "cube.jpg", "2월 17일"));
+        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "점심에 동네 떡볶이 맛집 갔다옴", "0", "cube.jpg", "2월 17일"));
+        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "오늘도 열심히 운동", "0", "cube.jpg", "2월 17일"));
+        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "안녕하세요", "0", "cube.jpg", "2월 17일"));
+        adapter.addItem(new Card(0, "서울시 강남구 홍길동", "0", ", ", ", ", "하빌리즘 입니다", "0", "cube.jpg", "2월 17일"));
 
         recyclerView.setAdapter(adapter);
         adapter.setOnItemClickListener(new OnCardItemClickListener() {
--- app/src/main/java/com/example/exampleapp/WriteFragment.java
+++ app/src/main/java/com/example/exampleapp/WriteFragment.java
@@ -1,19 +1,50 @@
 package com.example.exampleapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.channguyen.rsv.RangeSliderView;
+import com.google.android.material.tabs.TabLayout;
 
 public class WriteFragment extends Fragment {
+
+    Context context;
+    onTabItemSelectedListener listener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+
+        if(context instanceof onTabItemSelectedListener) { // context가
+            listener = (onTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if(context != null) {
+            context = null;
+            listener = null;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_list, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_write, container, false);
 
         initUi(rootView);
 
@@ -21,5 +52,44 @@ public class WriteFragment extends Fragment {
     }
     private void initUi(ViewGroup rootView) { // 인플레이션 후에 xml 레이아웃 안에 들어 있는 위젯이나 레이아웃을 찾아
         // 변수에 할당하는 코드들을 넣기 위해 만들어 둔 것임
+
+        Button saveBtn = rootView.findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null) {
+                    listener.onTabSelected(0); // 리스트 프래그먼트로 화면 전환
+                }
+            }
+        });
+
+        Button deleteBtn = rootView.findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null) {
+                    listener.onTabSelected(0); // 리스트 프래그먼트로 화면 전환
+                }
+            }
+        });
+
+        Button cancelBtn = rootView.findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null) {
+                    listener.onTabSelected(0); // 리스트 프래그먼트로 화면 전환
+                }
+            }
+        });
+
+        RangeSliderView sliderView = rootView.findViewById(R.id.sliderView);
+        sliderView.setOnSlideListener(new RangeSliderView.OnSlideListener() {
+            @Override
+            public void onSlide(int index) { // 값이 바뀔 때마다 호출
+
+            }
+        });
+        sliderView.setInitialIndex(2); // 다섯 개의 기분 중 가운데 기분이 디폴트 값임
     }
 }
